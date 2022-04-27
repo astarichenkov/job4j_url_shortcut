be0dc3dff7847086c64b76bc488fcc9dba95e8ca
Anton.Starichenkov <anton.starichenkov@gmail.com>
2022-04-28 01:15:12 +0300

    2. Сервис - UrlShortCut [#13799]

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,29 +1,29 @@
-create table sites
-(
-    id       bigserial
-        constraint sites_pkey
-            primary key,
-    login    varchar(255),
-    password varchar(255),
-    site     varchar(255)
-);
-
-alter table sites
-    owner to postgres;
-
-create table url_shortcut
-(
-    id      bigserial
-        constraint url_shortcut_pkey
-            primary key,
-    code    varchar(255),
-    count   bigint       not null,
-    url     varchar(255) not null,
-    site_id bigint
-        constraint fkme529jd16uaewkrdfr3fu2duj
-            references sites
-);
-
-alter table url_shortcut
-    owner to postgres;
-
+-- create table sites
+-- (
+--     id       bigserial
+--         constraint sites_pkey
+--             primary key,
+--     login    varchar(255),
+--     password varchar(255),
+--     site     varchar(255)
+-- );
+--
+-- alter table sites
+--     owner to postgres;
+--
+-- create table url_shortcut
+-- (
+--     id      bigserial
+--         constraint url_shortcut_pkey
+--             primary key,
+--     code    varchar(255),
+--     count   bigint       not null,
+--     url     varchar(255) not null,
+--     site_id bigint
+--         constraint fkme529jd16uaewkrdfr3fu2duj
+--             references sites
+-- );
+--
+-- alter table url_shortcut
+--     owner to postgres;
+--
